d7542afd838505581b6e5e4005f532040787fe66
Raymond Alvin <raymond1alvin@gmail.com>
2024-03-14 17:43:30 -0700

    added 3 month filter

--- rrid_project/rrid_project/spiders/gates.py
+++ rrid_project/rrid_project/spiders/gates.py
@@ -1,5 +1,6 @@
 import scrapy
 import time
+from datetime import datetime, timedelta
 from urllib.parse import urljoin
 
 
@@ -7,29 +8,31 @@ class GatesSpider(scrapy.Spider):
     name = 'gates'
     start_urls = ['https://gatesopenresearch.org/browse/articles?term0=Medical_and_health_sciences'] 
 
-
     custom_settings = {
         'FEED_FORMAT': 'csv',
         'FEED_URI': 'gates.csv',
     }
 
-    # TODO: figure out if get is sufficient for article funders vs getall
     def parse(self, response):
+        num_days = 90
         for articles in response.css('div.article-browse-wrapper'):
-            article_link = articles.css('a.article-link').attrib['href']
-            data = {
-                'link': article_link,
-                'publication_date': [text.split('PUBLISHED ')[1].strip() for text in response.css('div.article-bottom-bar::text').getall() if 'PUBLISHED ' in text][-1],
-                'title': articles.css('span.article-title::text').get(),
-                'authors': articles.css('span.js-article-author::text').getall(),
-                'article_funders': articles.css('span.article-funder-brand::text').getall(), 
-                'downloads': articles.css('span.article-metrics-wrapper::attr(data-downloads)').get(),
-                'views': articles.css('span.article-metrics-wrapper::attr(data-views)').get(),
-            }
-            yield scrapy.Request(url=article_link, callback=self.get_abstract, meta={'data': data})
+            publication_date_str = articles.css('div.article-bottom-bar::text').re_first('PUBLISHED (.+)')
+            publication_date = datetime.strptime(publication_date_str, '%d %b %Y')
+
+            if publication_date >= datetime.now() - timedelta(days=num_days):
+                article_link = articles.css('a.article-link').attrib['href']
+                data = {
+                    'link': article_link,
+                    'publication_date': publication_date_str,
+                    'title': articles.css('span.article-title::text').get(),
+                    'authors': articles.css('span.js-article-author::text').getall(),
+                    'article_funders': articles.css('span.article-funder-brand::text').getall(),
+                    'downloads': articles.css('span.article-metrics-wrapper::attr(data-downloads)').get(),
+                    'views': articles.css('span.article-metrics-wrapper::attr(data-views)').get(),
+                }
+                yield scrapy.Request(url=article_link, callback=self.get_abstract, meta={'data': data})
         
         next_page_url = response.css('a.js-pagination-next::attr(href)').get()
-  
         if next_page_url:
             next_page_url = urljoin(response.url, next_page_url)
             time.sleep(1)
